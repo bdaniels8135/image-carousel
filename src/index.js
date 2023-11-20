1d89a86bbbc5d00aeee02edad98efbaeaf56746c
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-19 20:34:40 -0500

    Build a demo of the image carousel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./index.css";
+
+import leafImg from "./img/leaf.jpg";
+import flowerImg from "./img/purple-coneflower.jpg";
+import ImageCarousel from "./ImageCarousel";
+
+const testImagesList = [leafImg, flowerImg];
+
+const imageCarousel = ImageCarousel(testImagesList);
+
+const body = document.querySelector("body");
+body.appendChild(imageCarousel);
