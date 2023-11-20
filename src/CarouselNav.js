59def9d3780c3af5886b0f9f925b930b21f53bf9
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-19 20:17:27 -0500

    Create a carousel nav object

---
+++ src/CarouselNav.js
@@ -0,0 +1,46 @@
+import leftArrowIcon from "./img/arrow-left-thick.svg";
+import rightArrowIcon from "./img/arrow-right-thick.svg";
+import { buildImgHtml, wrapHtmlElements } from "./htmlBuilders";
+
+const defaultRightBtnIcon = rightArrowIcon;
+const defaultLeftBtnIcon = leftArrowIcon;
+
+function buildNavDotHtml(clickFunc) {
+  const navDotHtml = document.createElement("div");
+  navDotHtml.addEventListener("click", (event) => {
+    clickFunc(event);
+  });
+  navDotHtml.classList.add("nav-dot");
+  return navDotHtml;
+}
+
+function buildNavDotsContainerHtml() {
+  const navDotsContainerHtml = document.createElement("div");
+  navDotsContainerHtml.classList.add("nav-dots-container");
+  return navDotsContainerHtml;
+}
+
+export default function CarouselNav(
+  leftBtnIcon = defaultLeftBtnIcon,
+  rightBtnIcon = defaultRightBtnIcon,
+) {
+  const leftBtnHtml = buildImgHtml(leftBtnIcon);
+  const rightBtnHtml = buildImgHtml(rightBtnIcon);
+  const navDotsContainerHtml = buildNavDotsContainerHtml();
+  const HTML = wrapHtmlElements(
+    "nav",
+    leftBtnHtml,
+    navDotsContainerHtml,
+    rightBtnHtml,
+  );
+
+  function appendNavDot(clickFunc) {
+    const newNavDotHtml = buildNavDotHtml(clickFunc);
+    navDotsContainerHtml.appendChild(newNavDotHtml);
+  }
+
+  return {
+    HTML,
+    appendNavDot,
+  };
+}
