60548863ddd145637dc0087e5747466d06e9bc30
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-19 21:49:35 -0500

    Assign the nav a class name

--- src/CarouselNav.js
+++ src/CarouselNav.js
@@ -33,6 +33,7 @@ export default function CarouselNav(
     navDotsContainerHtml,
     rightBtnHtml,
   );
+  HTML.classList.add("carousel-nav");
 
   function createNavDot(clickFunc) {
     const newNavDotHtml = buildNavDotHtml(clickFunc);
