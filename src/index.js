ff41029f49a31099db8efc716abb180eaca0bdb7
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-19 22:28:40 -0500

    Add a third test image

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import "./index.css";
 
 import leafImg from "./img/leaf.jpg";
 import flowerImg from "./img/purple-coneflower.jpg";
+import mountainImg from "./img/mountains.jpg";
 import ImageCarousel from "./ImageCarousel";
 
-const testImagesList = [leafImg, flowerImg];
+const testImagesList = [mountainImg, leafImg, flowerImg];
 
 const imageCarousel = ImageCarousel(testImagesList);
 
